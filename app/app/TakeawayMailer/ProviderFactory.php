0f865fe3088beec2c48cf74c4877a531ee74faf1
Nikolay Georgiev <ehwas503@gmail.com>
2020-09-17 13:59:19 +0300

    separate provider pickup logic in factory

---
+++ app/app/TakeawayMailer/ProviderFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\TakeawayMailer;
+
+use App\TakeawayMailer\Providers\MailjetProvider;
+use App\TakeawayMailer\Providers\SendgridProvider;
+
+class ProviderFactory
+{
+
+    private static $available_providers = [
+        SendgridProvider::class,
+        MailjetProvider::class,
+    ];  
+
+    public static function make(int $retry)
+    {
+        $key = $retry % count(self::$available_providers);
+        $provider = self::$available_providers[$key];
+        return new $provider;
+    }
+}
--- app/app/TakeawayMailer/TakeawayMailer.php
+++ app/app/TakeawayMailer/TakeawayMailer.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\TakeawayMailer;
+
 use App\TakeawayMailer\Message;
 use App\TakeawayMailer\Providers\SendgridProvider;
 use App\TakeawayMailer\Providers\MailjetProvider;
@@ -10,22 +11,10 @@ class TakeawayMailer
     private $provider;
     private $message;
 
-    private static $available_providers = [
-        SendgridProvider::class,
-        MailjetProvider::class,
-    ];    
-
     public function __construct(Message $message, int $retry)
     {
         $this->message = $message;
-        $this->provider = $this->pickProvider($retry);
-    }
-
-    private function pickProvider(int $retry)
-    {
-        $key = $retry % count(self::$available_providers);
-        $provider = self::$available_providers[$key];
-        return new $provider;
+        $this->provider = ProviderFactory::make($retry);
     }
 
     public static function trySend(Message $message, int $retry)
