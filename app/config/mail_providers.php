999ea2d5b4cc46f6d4d1e6cd73c049258a3f4576
Nikolay Georgiev <ehwas503@gmail.com>
2020-09-18 15:06:16 +0300

    Configure providers

--- app/config/mail_providers.php
+++ app/config/mail_providers.php
@@ -2,9 +2,9 @@
 
 return [
     // MailJet credentials
-    'MJ_APIKEY_PUBLIC' => 'todo',
-    'MJ_APIKEY_PRIVATE' => 'todo',
+    'MJ_APIKEY_PUBLIC' => '0945cf800e1e673e90bfccba84cf8e2f',
+    'MJ_APIKEY_PRIVATE' => '6f3ebb37a3793695864ae2ede65c792e',
 
     // Sendgrid credentials
-    'SENDGRID_API_KEY' => 'todo',
+    'SENDGRID_API_KEY' => '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',
 ];
